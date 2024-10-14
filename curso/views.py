f329bfa4747050bfbdfdd6f339bb83bc52f5ebb6
Paula Diaz <paulafdiazc@gmail.com>
2024-10-14 18:31:19 -0500

    Codigo finalizado

---
+++ curso/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from .models import curso
+from .forms import CursoForm
+
+# Vista para listar los cursos
+def get_curso(request):
+    cursos = curso.objects.all()
+    return render(request, 'lista_cursos.html', {
+        'title': 'Lista de cursos',
+        'cursos': cursos,
+    })
+
+# Vista para agregar o editar cursos
+def formulario(request):
+    if request.method == 'POST':
+        form = CursoForm(request.POST)
+        if form.is_valid():
+            form.save()  # Guarda el nuevo curso
+            return redirect('lista-cursos')  # Redirige a la página de lista de cursos después de guardar
+    else:
+        form = CursoForm()
+
+    return render(request, 'formulario_curso.html', {'form': form})
